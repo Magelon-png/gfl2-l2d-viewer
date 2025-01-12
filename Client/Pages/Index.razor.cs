09317a38a9eab54cb7c8f39dec53cd5f41c8a125
Magelon-png <Magelon-png>
2025-01-12 20:01:34 +0100

    Setup for github

---
+++ Client/Pages/Index.razor.cs
@@ -0,0 +1,145 @@
+﻿using System.Net.Http.Json;
+using System.Text.Json;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using MudBlazor;
+
+namespace GirlsFrontline2Live2DViewer.Client.Pages;
+
+public partial class Index
+{
+    [Inject] public IJSRuntime JsRuntime { get; set; }
+
+    [Inject] public HttpClient HttpClient { get; set; }
+    
+    [Inject] public ISnackbar Snackbar { get; set; }
+
+    private IJSObjectReference? module;
+    private Dictionary<string, string>? Live2DMapping = null;
+    public bool selectedModelEditable = false;
+    public string? selectedModel = null;
+    public string? selectedExpression = null;
+    public bool eyeTrackingEnabled = false;
+    public double modelScale;
+    public L2dModel? selectedModelObject;
+
+    protected override async Task OnInitializedAsync()
+    {
+        Live2DMapping = await HttpClient.GetFromJsonAsync<Dictionary<string, string>>("L2D/mapping.json", new JsonSerializerOptions()
+        {
+            ReadCommentHandling = JsonCommentHandling.Skip
+        });
+        Live2DMapping = Live2DMapping.OrderBy(x => x.Key).ToDictionary();
+    }
+
+    protected async override Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            module = await JsRuntime.InvokeAsync<IJSObjectReference>("import", "./Pages/Index.razor.js");
+        }
+    }
+
+    public async void OnSelectedModelChanged()
+    {
+        Console.WriteLine($"Selected model is now at {selectedModel}");
+        if (module is not null)
+        {
+            try
+            {
+                selectedModelObject = await module.InvokeAsync<L2dModel>("loadModel",
+                    $"L2D/{Live2DMapping[selectedModel]}/{Live2DMapping[selectedModel]}.model3.json",
+                    selectedModelEditable);
+                if (selectedModelObject is not null)
+                {
+                    Console.WriteLine("Selected model from js is not null");
+                    Console.WriteLine(selectedModelObject.Motions?.Count);
+                    Console.WriteLine(selectedModelObject.Expressions?.Length);
+                    modelScale = selectedModelObject.Scale;
+                    selectedExpression = null;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Snackbar.Add("Failed to load the selected model", Severity.Error);
+                selectedModelObject = null;
+                modelScale = 0;
+            }
+
+            await InvokeAsync(() => StateHasChanged());
+        }
+    }
+
+    public async void OnEyeTrackingEnabled()
+    {
+        Console.WriteLine($"Eye tracking set to {eyeTrackingEnabled}");
+
+        if (module is not null)
+        {
+            await module.InvokeVoidAsync("setEyeTracking", eyeTrackingEnabled);
+            await InvokeAsync(() => StateHasChanged());
+        }
+    }
+
+    public async void OnSelectedExpressionChanged()
+    {
+        Console.WriteLine($"Selected expression is now {selectedExpression}");
+        if (module is not null && selectedExpression is not null)
+        {
+            await module.InvokeVoidAsync("setExpression", selectedExpression);
+            await InvokeAsync(() => StateHasChanged());
+        }
+    }
+
+    public async Task OnMotionButtonClicked(string motion)
+    {
+        Console.WriteLine($"Selected motion {motion}");
+        if (module is not null)
+        {
+            await module.InvokeVoidAsync("setMotion", motion);
+            await InvokeAsync(() => StateHasChanged());
+        }
+    }
+    
+    private async void OnModelScaleChanged()
+    {
+        Console.WriteLine($"Changing scale to {modelScale}");
+        if (module is not null)
+        {
+            await module.InvokeVoidAsync("setScale", modelScale);
+            await InvokeAsync(() => StateHasChanged());
+        }
+    }
+
+    public async Task OnModelParameterChanged(string parameter, double value)
+    {
+        Console.WriteLine($"Changing parameter {parameter} to {value}");
+        if (module is not null)
+        {
+            await module.InvokeVoidAsync("setParameter", parameter, value);
+            await InvokeAsync(() => StateHasChanged());
+        }
+    }
+
+    async ValueTask IAsyncDisposable.DisposeAsync()
+    {
+        if (module is not null)
+        {
+            try
+            {
+                await module.DisposeAsync();
+            }
+            catch (JSDisconnectedException)
+            {
+            }
+        }
+    }
+
+    public record L2dExpression(string Name, string FilePath);
+
+    public record L2dMotion(string File);
+
+    public record L2dModel(Dictionary<string, L2dMotion[]>? Motions, L2dExpression[]? Expressions, double Scale, 
+        string[]? Parameters, bool Editable = false);
+}
---
+++ Client/Pages/Index.razor.js
@@ -0,0 +1,207 @@
+﻿class L2dMotion {
+    /**
+     *
+     * @param {string} file
+     */
+    constructor(file) {
+        this.file = file;
+    }
+}
+
+class L2dMotionDictionary {
+    /**
+     *
+     * @param {Object.<string, L2dMotion[]>} motions
+     */
+    constructor(motions) {
+        this.motions = motions;
+    }
+}
+
+class L2dExpression {
+    /**
+     *
+     * @param {string} name
+     * @param {string} filePath
+     */
+    constructor(name, filePath) {
+        this.name = name;
+        this.filePath = filePath;
+    }
+}
+
+class L2dModel {
+    /**
+     * 
+     * @param {L2dMotionDictionary?} motions 
+     * @param {L2dExpression[]?} expressions
+     * @param {number} scale
+     * @param {string[]} parameters
+     * @param {boolean} editable
+     */
+    constructor(motions, expressions, scale, parameters, editable) {
+        this.motions = motions;
+        this.expressions = expressions;
+        this.scale = scale;
+        this.parameters = parameters;
+        this.editable = editable;
+    }
+}
+
+/**
+ *
+ * @type {Application}
+ */
+var l2dApp;
+
+/**
+ * @type {Live2DModel}
+ */
+var l2dModel;
+
+
+/**
+ *
+ * @param {string} modelPath 
+ * @param {boolean} editable
+ * @returns {Promise<L2dModel>}
+ */
+export async function loadModel(modelPath, editable) {
+    console.log(`Attempting to load L2D model from ${modelPath}`);
+
+    l2dApp = new PIXI.Application({
+        view: document.getElementById("canvas"),
+        autoStart: true,
+        resizeTo: window
+    });
+
+    l2dModel = await PIXI.live2d.Live2DModel.from(modelPath);
+
+    l2dApp.stage.addChild(l2dModel);
+
+    //Disable eye blinking if loaded as editable
+    //If eye blinking is working => Cannot edit parameters
+    if(editable === true) {
+        l2dModel.internalModel.eyeBlink = undefined
+    }
+
+    l2dModel.interactive = false;
+
+    l2dModel.autoInteract = false;
+    
+
+    let scale = Math.min(l2dApp.view.width / l2dModel.internalModel.width, 
+        l2dApp.view.height / l2dModel.internalModel.height);
+
+    scale = Math.round(scale * 10) / 10;
+    
+    l2dModel.scale.set(scale, scale);
+    
+    const returnValue = new L2dModel(l2dModel.internalModel.settings?.motions,
+        l2dModel.internalModel.settings?.expressions, scale, 
+        l2dModel.internalModel.coreModel._parameterIds,
+        editable);
+    
+    console.log(JSON.stringify(returnValue));
+    
+    return returnValue;
+}
+
+/**
+ * 
+ * @param {string} expression
+ */
+export function setExpression(expression) {
+    if(l2dApp && l2dModel) {
+        l2dModel.expression(expression);
+    }
+}
+
+/**
+ * 
+ * @param {boolean} enabled
+ */
+export function setEyeTracking(enabled)
+{
+    if(l2dApp && l2dModel) {
+        l2dModel.interactive = enabled;
+
+        l2dModel.autoInteract = enabled;
+    }
+}
+
+/**
+ * 
+ * @param {string} motion
+ */
+export function setMotion(motion) {
+    if(l2dModel && l2dApp) {
+        l2dModel.motion(motion, 0);
+    }
+}
+
+/**
+ * 
+ * @param {string} scale
+ */
+export function setScale(scale) {
+    if(l2dModel && l2dApp) {
+        l2dModel.scale.set(scale);
+    }
+}
+
+/**
+ * @description Edit one of the parameters on the model. Can only be used if the model is loaded as editable (EyeBlink = undefined)
+ * @param {string} name
+ * @param {string} value
+ */
+export function setParameter(name, value) {
+    if(l2dModel && l2dApp) {
+        l2dModel.internalModel.coreModel.setParameterValueById(name, value);
+    }
+}
+
+
+//Dragging based on https://www.w3schools.com/howto/howto_js_draggable.asp
+dragElement(document.getElementById("canvas"));
+
+function dragElement(elmnt) {
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(elmnt.id + "header")) {
+        // if present, the header is where you move the DIV from:
+        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+        // otherwise, move the DIV from anywhere inside the DIV:
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        l2dModel.position.y = (l2dModel.position.y - pos2);
+        l2dModel.position.x = (l2dModel.position.x - pos1) ;
+    }
+
+    function closeDragElement() {
+        // stop moving when mouse button is released:
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
