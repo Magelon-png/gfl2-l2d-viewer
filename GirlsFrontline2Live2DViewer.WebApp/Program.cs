4f7117a77e53310ed6db4e96d0f05cc65146ac4f
Magelon-png <Magelon-png>
2025-01-13 12:19:51 +0100

    Update App to use blazor web app

---
+++ GirlsFrontline2Live2DViewer.WebApp/Program.cs
@@ -0,0 +1,50 @@
+using GirlsFrontline2Live2DViewer.WebApp.Components;
+using Microsoft.AspNetCore.Components;
+using MudBlazor.Services;
+
+namespace GirlsFrontline2Live2DViewer.WebApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+        builder.Services.AddRazorComponents()
+            .AddInteractiveServerComponents()
+            .AddInteractiveWebAssemblyComponents();
+        
+        builder.Services.AddScoped(sp => 
+        {
+            NavigationManager navigation = sp.GetRequiredService<NavigationManager>();
+            return new HttpClient() { BaseAddress = new Uri(navigation.BaseUri) };
+
+        });
+
+        
+        builder.Services.AddMudServices();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (!app.Environment.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAntiforgery();
+
+        app.MapStaticAssets();
+        app.MapRazorComponents<App>()
+            .AddInteractiveServerRenderMode()
+            .AddInteractiveWebAssemblyRenderMode()
+            .AddAdditionalAssemblies(typeof(Client._Imports).Assembly);;
+
+        app.Run();
+    }
+}
